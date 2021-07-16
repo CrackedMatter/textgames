3811cba0063fb04c22359f1b573eb6880779b4e3
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2021-07-16 18:49:41 +0200

    Update textgames.sh

--- src/textgames.sh
+++ src/textgames.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 TXGLINE=1
+TXGAME=$1
+[[ $1 == */* ]] || TXGAME=~/.txg/games/$1
 while [ $TXGLINE -ne 0 ]
 do
-  echo -e $(sed "${TXGLINE}q;d" $1)
-  TXGOPT=$(sed "$((TXGLINE+1))q;d" $1)
+  echo -e $(sed "${TXGLINE}q;d" $TXGAME)
+  TXGOPT=$(sed "$((TXGLINE+1))q;d" $TXGAME)
   echo [Options] $TXGOPT
   TXGREAD=""
   while [[ $TXGOPT != *$TXGREAD* ]] || [[ $TXGREAD == *,* ]] || [[ $TXGREAD == "" ]]
@@ -11,6 +13,6 @@ do
     read TXGREAD
   done
   TXGPOS=$(echo "$TXGOPT" | sed 's/'"$TXGREAD"'.*//' | tr -d -c , | wc -c)
-  TXGJUMP=$(sed "$((TXGLINE+2))q;d" $1)
+  TXGJUMP=$(sed "$((TXGLINE+2))q;d" $TXGAME)
   TXGLINE=$(echo $TXGJUMP | tr , '\n' | sed "$((TXGPOS+1))q;d")
 done
