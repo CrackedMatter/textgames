4ba95b21559e5188c3feba12b92125e202547cca
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2021-07-16 20:10:44 +0200

    Update textgames.sh

--- src/textgames.sh
+++ src/textgames.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 TXGLINE=1
 TXGAME=$1
-[[ $1 == */* ]] || TXGAME=~/.txg/games/$1
+echo "$1" | grep -q / || TXGAME=~/.txg/games/$1
 while [ $TXGLINE -ne 0 ]
 do
-  echo -e $(sed "${TXGLINE}q;d" $TXGAME)
+  printf "$(sed "$((TXGLINE))q;d" $TXGAME)"
+  echo
   TXGOPT=$(sed "$((TXGLINE+1))q;d" $TXGAME)
   echo [Options] $TXGOPT
   TXGREAD=""
-  while [[ $TXGOPT != *$TXGREAD* ]] || [[ $TXGREAD == *,* ]] || [[ $TXGREAD == "" ]]
+  while [ -z "$TXGREAD" ] || echo "$TXGREAD" | grep -q , || echo "$TXGOPT" | fgrep -qv "$TXGREAD"
   do
     read TXGREAD
   done
